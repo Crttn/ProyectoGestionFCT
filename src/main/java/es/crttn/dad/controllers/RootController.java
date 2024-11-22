bd5347bf858d1a1cfa866287e61aa42bdb04e0ea
crttn <crttngr@gmail.com>
2024-11-22 12:39:03 +0000

    Initial commit

---
+++ src/main/java/es/crttn/dad/controllers/RootController.java
@@ -0,0 +1,14 @@
+package es.crttn.dad.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
