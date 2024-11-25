26c5eedc9b578d1e58959d57610086060fa8729b
IgnacioToledo21 <ignaciosurf124@gmail.com>
2024-11-25 08:26:47 +0000

    Added Features
    .

---
+++ src/main/java/es/crttn/dad/controllers/GestionEmpresasController.java
@@ -0,0 +1,49 @@
+package es.crttn.dad.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GestionEmpresasController implements Initializable {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button findButton;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    void onAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onFindButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateButton(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+}
