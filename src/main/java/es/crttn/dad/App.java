2c491e935bd857cb3099fd045d5546b18fcbb4be
crttn <crttngr@gmail.com>
2024-11-25 08:25:11 +0000

    Update RootController

--- src/main/java/es/crttn/dad/App.java
+++ src/main/java/es/crttn/dad/App.java
@@ -1,15 +1,19 @@
 package es.crttn.dad;
 
+import es.crttn.dad.controllers.RootController;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application{
+
+    RootController rootController = new RootController();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("Proyecto Gestión FCT");
-        //primaryStage.setScene();
+        primaryStage.setScene(new Scene(rootController.getRoot()));
         primaryStage.show();
     }
 }
--- src/main/java/es/crttn/dad/controllers/RootController.java
+++ src/main/java/es/crttn/dad/controllers/RootController.java
@@ -1,14 +1,74 @@
 package es.crttn.dad.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
 
+    @FXML
+    private BorderPane root;
+
+
+    public RootController() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml"));
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+    public BorderPane getRoot() {
+        return root;
+    }
+
+    @FXML
+    void onAsignacionPracticasAction(ActionEvent event) throws IOException {
+
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml"));
+        Scene scene = new Scene(root);
+        stage = new Stage(StageStyle.DECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void onGestionAlumnosAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGestionComentariosAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGestionEmpresasAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGestionVisitasAction(ActionEvent event) {
 
     }
 }
