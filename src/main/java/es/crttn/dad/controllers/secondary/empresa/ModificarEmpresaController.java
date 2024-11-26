8d6e858e11dcaf97989deb8a0bd5fe18d6e0f3ca
crttn <crttngr@gmail.com>
2024-11-26 13:30:55 +0000

    Upadte ModificarEmpresaController

--- src/main/java/es/crttn/dad/controllers/secondary/empresa/ModificarEmpresaController.java
+++ src/main/java/es/crttn/dad/controllers/secondary/empresa/ModificarEmpresaController.java
@@ -2,6 +2,8 @@ package es.crttn.dad.controllers.secondary.empresa;
 
 import es.crttn.dad.App;
 import es.crttn.dad.DatabaseManager;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
@@ -10,24 +12,48 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.util.converter.NumberStringConverter;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class ModificarEmpresaController implements Initializable {
 
-    private ModificarEmpresa1Controller me1c = new ModificarEmpresa1Controller();
+    @FXML
+    private TextField correoTextField;
 
-    private final StringProperty nombreProperty = new SimpleStringProperty();
+    @FXML
+    private TextField direccionTextField;
+
+    @FXML
+    private TextField especialidadTextFiedl;
+
+    @FXML
+    private TextField horarioTextField;
 
     @FXML
     private TextField nombreEmpresaTextField;
 
     @FXML
+    private TextField nombreTextField;
+
+    @FXML
+    private TextField plazasTextField;
+
+    @FXML
     private BorderPane root;
+
+    private final StringProperty nombreProperty = new SimpleStringProperty();
+    private final StringProperty direccionProperty = new SimpleStringProperty();
+    private final StringProperty correoProperty = new SimpleStringProperty();
+    private final StringProperty horarioProperty = new SimpleStringProperty();
+    private final IntegerProperty plazasProperty = new SimpleIntegerProperty();
+    private final StringProperty especialidadProperty = new SimpleStringProperty();
+
 
     public ModificarEmpresaController() {
         try {
@@ -41,7 +67,14 @@ public class ModificarEmpresaController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         nombreEmpresaTextField.textProperty().bindBidirectional(nombreProperty);
+        direccionTextField.textProperty().bindBidirectional(direccionProperty);
+        correoTextField.textProperty().bindBidirectional(correoProperty);
+        horarioTextField.textProperty().bindBidirectional(horarioProperty);
+        plazasTextField.textProperty().bindBidirectional(plazasProperty, new NumberStringConverter());
+        especialidadTextFiedl.textProperty().bindBidirectional(especialidadProperty);
+
 
     }
 
@@ -51,16 +84,34 @@ public class ModificarEmpresaController implements Initializable {
 
     @FXML
     void onSearchAction(ActionEvent event) {
-        String querry = "SELECT * FROM alumno where dni = ?";
+        String querry = "select nombre,direccion, correo, horario, plazas_disp, especialidad from empresa WHERE nombre = ?";
+
 
         if (nombreProperty.getValue() != null) {
-            try (Connection connection = DatabaseManager.getDataSource().getConnection();
-                 PreparedStatement statement = connection.prepareStatement(querry)) {
+            try (Connection connection = DatabaseManager.getDataSource().getConnection(); PreparedStatement statement = connection.prepareStatement(querry)) {
 
                 statement.setString(1, nombreProperty.getValue());
 
-                App.getRootController().getRoot().setCenter(App.getRootController().getGestionMainController().getGec().getRoot());
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
 
+                        nombreProperty.setValue(resultSet.getString("nombre"));
+                        direccionProperty.setValue(resultSet.getString("direccion"));
+                        correoProperty.setValue(resultSet.getString("horario"));
+                        horarioProperty.setValue(resultSet.getString("horario"));
+                        plazasProperty.setValue(resultSet.getInt("plazas_disp"));
+                        especialidadProperty.setValue(resultSet.getString("especialidad"));
+
+
+                    }
+                }
+
+                nombreTextField.setText(nombreProperty.getValue());
+                direccionTextField.setText(direccionProperty.getValue());
+                correoTextField.setText(correoProperty.getValue());
+                horarioTextField.setText(horarioProperty.getValue());
+                plazasTextField.setText(plazasProperty.getValue().toString());
+                especialidadTextFiedl.setText(especialidadProperty.getValue());
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -69,15 +120,14 @@ public class ModificarEmpresaController implements Initializable {
     }
 
     @FXML
+    void onModifyAction(ActionEvent event) {
+        String querry = "UPDATE empresa SET nombre = ?, direccion = ?, correo = ?, horario = ? plazas_disp = ?, especialidad = ? WHERE nombre = ?";
+
+    }
+
+    @FXML
     void onBackaction(ActionEvent event) {
         App.getRootController().getRoot().setCenter(App.getRootController().getGestionMainController().getGec().getRoot());
-    }
 
-    public String nombreEmpresa() {
-        return nombreProperty.getValue();
-    }
-
-    public ModificarEmpresa1Controller getMe1c() {
-        return me1c;
     }
 }
