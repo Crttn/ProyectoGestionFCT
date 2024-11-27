0adf6ae0e2029e4fdc34f2f08604f02b61ca4b58
IgnacioToledo21 <ignaciosurf124@gmail.com>
2024-11-27 13:49:17 +0000

    EliminarVisitaController Updated

--- src/main/java/es/crttn/dad/controllers/secondary/visitas/EliminarVisitaController.java
+++ src/main/java/es/crttn/dad/controllers/secondary/visitas/EliminarVisitaController.java
@@ -1,24 +1,34 @@
 package es.crttn.dad.controllers.secondary.visitas;
 
 import es.crttn.dad.App;
+import es.crttn.dad.DatabaseManager;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EliminarVisitaController implements Initializable {
 
     @FXML
-    private TextField AsignacionTextfield;
+    private TextField idvisitaTextfield;
 
     @FXML
     private BorderPane root;
+
+    private StringProperty visitaProperty = new SimpleStringProperty();
 
     public EliminarVisitaController() {
         try {
@@ -33,6 +43,8 @@ public class EliminarVisitaController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        idvisitaTextfield.textProperty().bindBidirectional(visitaProperty);
+
     }
 
     public BorderPane getRoot() {
@@ -41,6 +53,31 @@ public class EliminarVisitaController implements Initializable {
 
     @FXML
     void onDeleteAction(ActionEvent event) {
+
+        if (visitaProperty != null) {
+            Alert deleteAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            deleteAlert.setTitle("Eliminar Visita");
+            deleteAlert.setHeaderText("¿Estás seguro de que deseas eliminar la visita " + visitaProperty.getValue() +  "?");
+            Optional<ButtonType> buttonType = deleteAlert.showAndWait();
+
+            if(buttonType.isPresent() && buttonType.get().equals(ButtonType.OK)) {
+                String querry = "DELETE FROM visitaseguimiento WHERE id_visita = ?";
+
+                try (Connection connection = DatabaseManager.getDataSource().getConnection();
+                     PreparedStatement statement = connection.prepareStatement(querry)) {
+
+                    // Usar los valores obtenidos de las propiedades
+                    statement.setString(1, visitaProperty.getValue());
+
+                    int filasAfectadas = statement.executeUpdate();
+                    System.out.println("Visita eliminada: " + filasAfectadas + " fila(s) afectada(s)");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                visitaProperty.setValue("");
+            }
+        }
 
     }
 
